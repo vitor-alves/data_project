e6bbbd97c5c2858e7468ca04444a9c3f8479bddd
vitor-alves <vitoralves842@gmail.com>
2022-10-12 17:31:00 -0300

    README minor update

--- mysql/sql_queries/weekly_average_num_trips.sql
+++ mysql/sql_queries/weekly_average_num_trips.sql
@@ -1,5 +1,6 @@
-prepare stmt from 'SELECT *
-FROM trips
+prepare stmt from 'with trips_per_week as (
+    select count(*) num_trips, week(datetime) week
+    from trips
 WHERE
 (
     -- Contains the region
@@ -16,5 +17,8 @@ OR
         POLYGON(LINESTRING(point(?,?),point(?,?),point(?,?),point(?,?),point(?,?))),
             destination_coord)
 )
-;';
+    group by week(datetime)
+)
+select sum(num_trips)/count(*) as "Average Trips per Week"
+from trips_per_week;';
 execute stmt using @region, @lon1, @lat1, @lon2, @lat2, @lon3, @lat3, @lon4, @lat4, @lon1, @lat1, @lon1, @lat1, @lon2, @lat2, @lon3, @lat3, @lon4, @lat4, @lon1, @lat1;
